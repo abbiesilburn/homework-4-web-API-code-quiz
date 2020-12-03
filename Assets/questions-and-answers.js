d031c21c46cbaf8dff59a2dea76adb80fcbbd1de
abbiesilburn <abbie.barker@hotmail.com>
2020-12-03 20:35:04 +0800

    wrote out all questions in js file, added nav bar into the html with link to high scores and shows time left

---
+++ Assets/questions-and-answers.js
@@ -0,0 +1,38 @@
+//create variable for list of questions 
+//create variable for question, possible answers and correct answer
+
+//Question 1
+"Which of the following is a javascript file?"
+
+"document.java", "info.j", "script.js", "index.script"
+
+//Question 2
+"What character do you need at the end of this javascript: document.write(1+2)"
+
+";", ":", ",","~"
+
+//Question 3
+"What does DOM stand for?"
+
+"Document Object Model", "Dynamic Oriented Mode", "Direct Object Maker", "Dynamic Object Maker"
+
+//Question 4
+"Where does the <script> go in a HTML document?"
+
+"Only in the Body", "Only in the Head", "Either the head or the body", "it cant't go in a HTML"
+
+//Question 5
+
+"How do you write a 'does not equal' sign in javascript?"
+
+"==/==", "!=", "x=", "DNE"
+
+//Question 6
+"What would most likely be at the start of a for-loop?"
+
+"(i=0, i>length, i++)", "(loop i for each iteration)", "i=0, i++", "(i = loop)"
+
+//Question 7
+"Which of these is an array?"
+
+"()", "{}", "[]", "//"
